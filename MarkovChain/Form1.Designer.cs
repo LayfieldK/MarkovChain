5d54d47cbe86e8a2c05127693fab09b122702e61
layfieldk <kevinjlayfield@gmail.com>
2016-04-08 18:59:44 -0400

    Initial

--- MarkovChain/Form1.Designer.cs
+++ MarkovChain/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AnalyzeButton = new System.Windows.Forms.Button();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.Location = new System.Drawing.Point(271, 305);
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Size = new System.Drawing.Size(147, 23);
+            this.AnalyzeButton.TabIndex = 0;
+            this.AnalyzeButton.Text = "Analyze Markov Chain";
+            this.AnalyzeButton.UseVisualStyleBackColor = true;
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Location = new System.Drawing.Point(324, 13);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(31, 13);
+            this.InputLabel.TabIndex = 1;
+            this.InputLabel.Text = "Input";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(60, 64);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(594, 235);
+            this.InputBox.TabIndex = 2;
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(60, 374);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(594, 151);
+            this.OutputBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 537);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.AnalyzeButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AnalyzeButton;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
